266a200f710ba7859a4b44a93796e2126d1a786a
yehia ismael <yoyo200_868@hotmail.com>
2023-06-24 20:47:20 +0300

    first commit

---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "antd";
+
+const Exchanges = () => {
+  return (
+    <div>
+      <Typography.Title level={1}>M3eesh flos a3mlo</Typography.Title>
+    </div>
+  );
+};
+
+export default Exchanges;
